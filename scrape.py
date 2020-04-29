570fee5791f4b3d53710453d471d609e64649c64
Kevin Pfeil <kevinpfeil@Kevins-MacBook-Pro.local>
2020-04-28 17:54:13 -0700

    names formatted

--- scrape.py
+++ scrape.py
@@ -15,7 +15,7 @@ top400 = []
 csv_file = open('top400_scrape.csv', 'w')
 
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow('Name')
+csv_writer.writerow(['Name'])
 
 # find all td (table data) elements in the html
 for td in soup.find_all('td'):
@@ -23,13 +23,13 @@ for td in soup.find_all('td'):
         # if there is not an anchor tag, essentially skip
         name = None
     else:
-        player_name = str(td.a.text)
+        player_name = td.a.text
         #print(player_name)
-        top400.append(player_name)
+        #top400.append(player_name)
+        csv_writer.writerow([player_name.encode('utf-8')])
 
-for i in range(len(top400)):
-        print(str(top400[i]))
-        csv_writer.writerow(top400[i])
+#for i in range(len(top400)):
+        #print(top400[i])
 
-#print(top400[200])
+
 csv_file.close()
