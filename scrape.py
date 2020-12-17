390eb7102a8ca6cfc973e51161259717c3caca45
unknown <kevpfeil25@gmail.com>
2020-12-16 17:58:17 -0800

    added new scrape

--- scrape.py
+++ scrape.py
@@ -5,11 +5,11 @@ import csv
 ###############################################################################
 # Names
 ###############################################################################
-source = requests.get('http://tennisabstract.com/reports/atp_elo_ratings.html').text
+source = requests.get('https://www.espn.com/tennis/rankings').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-# print(soup.prettify())
+#print(soup.prettify())
 top400 = []
 
 csv_file = open('top400_scrape.csv', 'w')
@@ -17,19 +17,23 @@ csv_file = open('top400_scrape.csv', 'w')
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['Name'])
 
-# find all td (table data) elements in the html
-for td in soup.find_all('td'):
-    if td.a == None:
-        # if there is not an anchor tag, essentially skip
-        name = None
-    else:
-        player_name = td.a.text
+# find all a tags where the class is AnchorLink, BUT theres an issue
+# there are many many a tags with this class
+# hence the logical ~ maze ~
+num_players = 0
+for tr in soup.find_all("a", class_= "AnchorLink"):
+    num_spaces = 0
+    player_name = tr.text
+    for letter in player_name:
+        if letter == " ":
+            num_spaces += 1
+    if (num_spaces == 1 or num_spaces == 2) and num_players < 100 and player_name != "Grand Slam History":
         #print(player_name)
-        #top400.append(player_name)
-        csv_writer.writerow([player_name.encode('utf-8')])
+        num_players += 1
+        top400.append(player_name)
+        csv_writer.writerow([player_name])
 
 #for i in range(len(top400)):
         #print(top400[i])
-
 
 csv_file.close()
